6f730833607cec4e060a53ed912829182772b06e
DovudSalimov668 <a9059262614@gmail.com>
2025-11-10 08:35:10 +0500

    paceholder update for verify code

--- test_smtp.py
+++ test_smtp.py
@@ -8,7 +8,7 @@ from notifications.email import send_simple_email_async
 # Test the email system
 send_simple_email_async(
     subject="Test Brevo API",
-    message="Your test OTP is: <div class='otp'>123456</div>This is a test.",
+    message="Your test OTP is: <div class='otp'> example: 123456</div>This is a test.",
     recipient_email="ggvpby6996@gmail.com"
 )
 print("Test email sent!")
